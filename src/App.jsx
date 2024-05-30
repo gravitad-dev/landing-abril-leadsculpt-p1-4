88e40d1bb84c6461157e7c3cfcccaee43b9d8fd2
silvak <silvak.jeg@gmail.com>
2024-05-30 12:03:47 -0400

    react iframe deploy

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='frameContainer'>
+      <iframe src="https://landing-april-leadsculpt-p1-4-next.vercel.app/" frameborder="0" ></iframe>
+    </div>
+  )
+}
+
+export default App
